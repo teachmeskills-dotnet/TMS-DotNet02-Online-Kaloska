9e9015d820b71f6b92c2ed7eb50584a1ba9a12ec
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-05-12 17:47:11 +0300

    feat : add models and Identity.EntityFrameworkCore

---
+++ src/TMS-DotNet02-Online-Kaloska.TmTracker.Data/Models/Goal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_DotNet02_Online_Kaloska.TmTracker.Data.Models
+{
+    /// <summary>
+    /// Goal.
+    /// </summary>
+    class Goal
+    {
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Project identifier.
+        /// </summary>
+        public int ProjectId { get; set; }
+        /// <summary>
+        /// Navigation property for project.
+        /// </summary>
+        public Project Project { get; set; }
+        /// <summary>
+        /// Title.
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Text.
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Is complete
+        /// </summary>
+        public bool IsComplete { get; set; }
+        /// <summary>
+        /// Start.
+        /// </summary>
+        public DateTime Start { get; set; }
+        /// <summary>
+        /// End.
+        /// </summary>
+        public DateTime End { get; set; }
+    }
+}
---
+++ src/TMS-DotNet02-Online-Kaloska.TmTracker.Data/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_DotNet02_Online_Kaloska.TmTracker.Data.Models
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    class User : IdentityUser
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Navigation property for record.
+        /// </summary>
+        public ICollection<Record> Records { get; set; }
+        /// <summary>
+        /// Navigation property for user.
+        /// </summary>
+        public ICollection<Project> Projects { get; set; }
+    }
+}
